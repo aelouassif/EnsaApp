87bb2bebe50cb6bfd1079ec2ebf49d2f94a30ac5
Yassir Karroum <ukarroum17@gmail.com>
2016-03-01 19:27:26 +0000

    Ajout du choix du niveau ajout de deux images representants les cycle preparatoirs lors du choix du niveau Le code Js doit etre revue

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -1,3 +1,15 @@
+/*
+    ToDo
+    ====
+Le fichier doit etre découpé et mieux organisé parceque là c'est vraiment le bordel :x
+
+Et je dois me faire un plan clair sinon je vais vite me retrouver avec un code duppliqué partout 
+
+La structure est à revoir donc .
+
+*/
+
+
 // Ionic Starter App
 
 //Configuration à modifier si vous voulez déployer ce script
@@ -25,6 +37,7 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
 .controller('FilieresController', function($scope, $http, $timeout) 
 {
     $scope.rootServ = rootServ;
@@ -37,5 +50,65 @@ angular.module('starter', ['ionic'])
         {
             console.error('ERR', err);
         });
-});
+    $scope.setFiliere = function(id)
+    {
+        window.localStorage['idFiliere'] = id;
+    };
+})
+
+.controller('NiveauController', function($scope, $http, $timeout) 
+{
+    $scope.rootServ = rootServ;
+    $scope.data = [];
+    $http.get('http://localhost/EnsaApp/server/niveaux.php?id=' + window.localStorage['idFiliere']).then(function(resp) 
+        {
+            $scope.data = resp.data;
+
+            for(var i = 0; i < $scope.data.length; i++)
+            {
+                switch($scope.data[i]['lvl'])
+                {
+                    case "1":
+                        $scope.data[i]['nom'] = 'Première année';
+                        break;
+                    case "2":
+                        $scope.data[i]['nom'] = 'Deuxième année';
+                        break;
+                    case "3":
+                        $scope.data[i]['nom'] = 'Troisième année';
+                        break;
+                    default:
+                        $scope.data[i]['nom'] = 'X éme année'; //Si ca s'affiche c'est que j'ai mérdé quelquepart :-° 
+                        break;
+                }
+            }
+        },
+        function(err) 
+        {
+            console.error('ERR', err);
+        });
+    for(var i = 0; i < $scope.data.length; i++)
+    {
+        switch($scope.data[i]['lvl'])
+        {
+            case 1:
+                $scope.data[i]['nom'] = 'Première année';
+                break;
+            case 2:
+                $scope.data[i]['nom'] = 'Deuxième année';
+                break;
+            case 3:
+                $scope.data[i]['nom'] = 'Troisième année';
+                break;
+            default:
+                $scope.data[i]['nom'] = 'X éme année'; //Si ca s'affiche c'est que j'ai mérdé quelquepart :-° 
+                break;
+        }
+    }
+            
+    $scope.setNiveau = function(id)
+    {
+        window.localStorage['idNiveau'] = id;
+    };
+})
 
--- server/niveaux.php
+++ server/niveaux.php
@@ -1,7 +1,9 @@
 <?php
     require_once("connect.php");
-    
-    $req = $bdd->query("SELECT * FROM niveau");
+    if(isset($_GET['id']) && is_numeric($_GET['id']) && $_GET['id'] > 0)
+        $req = $bdd->query("SELECT * FROM niveau WHERE filiereId=".$_GET['id']);
+    else
+        $req = $bdd->query("SELECT * FROM niveau");
 
     $myArray = array();
     while($row = $req->fetch_array(MYSQL_ASSOC)) 
